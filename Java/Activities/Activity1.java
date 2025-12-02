54326edb38c396e385715af34ffc90e766481e76
VindhyaPentaparthy315 <Vindhya.Pentaparthy@ibm.com>
2025-12-02 12:05:18 +0530

    Add files via upload

---
+++ Java/Activities/Activity1.java
@@ -0,0 +1,15 @@
+package Examples;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		Car carName = new Car();// TODO Auto-generated method stub
+		carName.make=2014;
+		carName.color="Black";
+		carName.transmission="Manual";
+		carName.displayCharacteristics();
+		carName.accelarate();
+		carName.brake();
+	}
+
+}
