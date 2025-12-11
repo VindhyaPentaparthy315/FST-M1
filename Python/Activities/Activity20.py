bf084396c6fa292c79a97b18459ff65390549a15
VindhyaPentaparthy315 <Vindhya.Pentaparthy@ibm.com>
2025-12-11 11:24:31 +0530

    Add files via upload

---
+++ Python/Activities/Activity20.py
@@ -0,0 +1,11 @@
+import pandas
+dataframe = pandas.read_excel('sample.xlsx')
+
+print(dataframe)
+print("Number of rows and columns:", dataframe.shape)
+
+print("Emails:")
+print(dataframe['Email'])
+
+print("Sorted data:")
+print(dataframe.sort_values('FirstName'))
